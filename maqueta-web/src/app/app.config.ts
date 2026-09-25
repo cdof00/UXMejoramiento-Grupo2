c0f1cb6a785833c0b9ea8d0cb87d76f9cd12f4ca
Steven Garzon <sr.garzon@uniandes.edu.co>
2026-09-24 22:17:02 -0500

    QA: migra iconos a Material Symbols (MD3) y ajusta icono de pausa

--- maqueta-web/src/app/app.config.ts
+++ maqueta-web/src/app/app.config.ts
@@ -1,6 +1,7 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { APP_INITIALIZER, ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { MatIconRegistry } from '@angular/material/icon';
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
@@ -8,5 +9,12 @@ export const appConfig: ApplicationConfig = {
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     provideAnimationsAsync(),
+    {
+      provide: APP_INITIALIZER,
+      useFactory: (registry: MatIconRegistry) => () =>
+        registry.setDefaultFontSetClass('material-symbols-outlined'),
+      deps: [MatIconRegistry],
+      multi: true,
+    },
   ],
 };
